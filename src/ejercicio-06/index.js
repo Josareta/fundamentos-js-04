a208602fb1256141daa7ad19303793c590f23088
Josareta <josareta@gmail.com>
2023-09-29 16:29:03 +0200

    Añadir Ejercicio 06

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,22 @@
-// Escribe tu código aquí:
+// Herencia de clases
+
+class rectangulo {
+    constructor(ancho, alto){
+        this.ancho = ancho
+        this.alto = alto
+    }
+    calculaArea(){
+        return this.ancho * this.alto
+    }
+}
+class cuadrado extends rectangulo{
+    constructor(caraLarga) {
+        super(caraLarga, caraLarga)
+    }
+}
+
+const Cuadrado = new cuadrado(9)
+
+const print = console.log
+
+print("Area del cuadrado:",Cuadrado.calculaArea())
